e8d57263c7a4a20dbf259bee2a7068f14bc3ca2a
Brad Hughes <Oricul@users.noreply.github.com>
2023-07-14 03:14:54 -0500

    fix: resize text shutdown/formatting

--- mods/resize-text.user.js
+++ mods/resize-text.user.js
@@ -14,52 +14,62 @@
 // ==/UserScript==
 
 function resizeText() {
-    const comment = document.querySelectorAll('blockquote.comment div');
-    const posts = document.querySelectorAll('div.content.formatted');
-    let settings = getModSettings("resize");
-    const sizes = {
-        "Small": ".8rem",
-        "Normal": ".9rem",
-        "Large": "1rem",
-        "Extra Large": "1.1rem",
-        "Extra Extra Large": "1.3rem"
-      };
-    const selectedSize = settings["size"];
-    const sizeValue = sizes[selectedSize];
+  const comment = document.querySelectorAll('blockquote.comment div');
+  const posts = document.querySelectorAll('div.content.formatted');
+  let settings = getModSettings("resize");
+  const sizes = {
+      "Small": ".8rem",
+      "Normal": ".9rem",
+      "Large": "1rem",
+      "Extra Large": "1.1rem",
+      "Extra Extra Large": "1.3rem"
+    };
+  const selectedSize = settings["size"];
+  const sizeValue = sizes[selectedSize];
+
+  // call functions to modify text sizes depending on application choice
+    if (settings["option"] == "Only comments") {
+        resizeComments();
+    } else if (settings["option"] == "Only posts") {
+        resizePosts();
+    } else if (settings["option"] == "Comments and posts") {
+        resizeComments();
+        resizePosts();
+    }
+
+  // functions that modify text sizes based on choice
+  function resizeComments() {   
+      function iterateComments(param, size) {
+        param.forEach(comment => {
+          comment.style.fontSize = size;
+        });
+      }
     
-    // call functions to modify text sizes depending on application choice
-      if (settings["option"] == "Only comments") {
-          resizeComments();
-      } else if (settings["option"] == "Only posts") {
-          resizePosts();
-      } else if (settings["option"] == "Comments and posts") {
-          resizeComments();
-          resizePosts();
-      }
-  
-    // functions that modify text sizes based on choice
-    function resizeComments() {   
-        function iterateComments(param, size) {
-          param.forEach(comment => {
-            comment.style.fontSize = size;
-          });
-        }
-      
-        iterateComments(comment, sizeValue);
-    }
-  
-    function resizePosts() {
-      posts.forEach(post => {
-        post.style.fontSize = sizeValue;
-      })
-    }
+      iterateComments(comment, sizeValue);
   }
-  
-  
-  function textResize(toggle) {
-      if (toggle) {
-          resizeText();
-      } else {
-      }
+
+  function resizePosts() {
+    posts.forEach(post => {
+      post.style.fontSize = sizeValue;
+    })
   }
-  
+}
+
+function resizeTextShutdown() {
+  const comments = document.querySelectorAll('blockquote.comment div');
+  const posts = document.querySelectorAll('div.content.formatted');
+  comments.forEach(comment => {
+    comment.style.fontSize = '.9rem';
+  });
+  posts.forEach(post => {
+    post.style.fontSize = '.9rem';
+  });
+}
+
+function textResize(toggle) {
+  if (toggle) {
+      resizeText();
+  } else {
+    resizeTextShutdown();
+  }
+}
